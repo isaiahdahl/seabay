626af917a905f182dc97bfcee2960e7355de8d11
Guillaume Barthere <gbarthere@yahoo.com>
2017-11-14 17:58:17 +0000

    The navbar content adapts if the current_user is restaurant owner or not.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def is_restaurant_owner?
+    @restaurants = Restaurant.all
+    @restaurants.each do |restaurant|
+      if restaurant.user_id == current_user.id
+        return restaurant
+      end
+    end
+  end
+
 end
