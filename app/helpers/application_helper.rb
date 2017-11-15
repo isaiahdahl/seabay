5ee1a393cc76b6d08acedbcd59c639914d0cdc6f
Jeremy Afuso <jeremy@afusodesign.com>
2017-11-15 13:22:57 +0000

    fixed buttons and resto form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     @resto = Restaurant.where( user_id: current_user.id)
   end
 
+  def photo?(elm)
+    if elm.img_url?
+      elm.img_url
+    else
+      image_path "fisherman.jpg"
+    end
+  end
+
 end
