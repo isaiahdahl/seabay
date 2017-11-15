e2fbed9727b70df54423ba1b06034fa7ecf06e3c
Guillaume Barthere <gbarthere@yahoo.com>
2017-11-15 11:15:38 +0000

    Killed the my_restaurants path. Also now the navbar react correctly if the user is login or not and if the user is a restaurant owner or not

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
 
   def is_restaurant_owner?
-    @resto = Restaurant.where( user_id: current_user.id)
+    @resto = Restaurant.all
+    my_restaurant = []
+    @resto.each do |restaurant|
+      if restaurant.user_id == current_user.id
+        my_restaurant << restaurant
+      end
+    end
+    my_restaurant.first
   end
-
 end
